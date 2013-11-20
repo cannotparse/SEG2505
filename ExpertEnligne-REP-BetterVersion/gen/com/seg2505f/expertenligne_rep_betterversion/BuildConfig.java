94cc9e1a63b379989fd52778bdce398c9b1e1159
Rodger <rwald@uocsclub.ca>
2013-11-19 20:34:38 -0500

    Added Fixed version of the Profs material
    You can now import this project into your work space. It still crashes
    however. I suspect it has to do with swarm. Setting up log cat.

---
+++ ExpertEnligne-REP-BetterVersion/gen/com/seg2505f/expertenligne_rep_betterversion/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.seg2505f.expertenligne_rep_betterversion;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
